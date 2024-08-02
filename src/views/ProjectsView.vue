16e07a4805ba63336e120ef7938353ab3bdf33d6
Jason September <147480420+JsonSept@users.noreply.github.com>
2024-08-02 11:43:10 +0200

    Update ProjectsView.vue

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -3,127 +3,58 @@
         <h1 class="pro">My Projects</h1>
         <div class="container">
         <div class="card">
-            <div class="content">
-                <h2>01</h2>
-                <img class="img0" src="https://i.postimg.cc/MpG4jZww/Screenshot-2024-04-05-150844.png">
-                <h3>Sneaker Shop</h3>
-                <p>A sneaker shop website made with HTML and CSS</p>
-                <a href="https://github.com/JsonSept/week1.git" target="_blank">
-                    <button>
-                    </button>
-                    Github
-                </a><br>
-            <a href="https://adorable-churros-ec7e34.netlify.app" target="_blank">
-                    <button>    
-                    </button>
-                    Netlify
+                <div class="content">
+                    <h2>01</h2>
+                    <img class="img0" src="https://i.postimg.cc/TYjQHSVp/Screenshot-2024-04-05-151117.png"
+                        style="width: 230px;">
+                    <h3>Keyboard</h3>
+                    <p>Made with HTML, CSS and JavaScript</p>
+                    <a href="https://github.com/JsonSept/jsKeyboard.git" target="_blank">
+                        <button>
+                        </button>
+                        Github
+                    </a><br>
+                    <a href="https://wonderful-blini-2d38a2.netlify.app" target="_blank">
+                        <button>
+                        </button>
+                        Netlify
                     </a>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <div class="content">
-                <h2>02</h2>
-                <img class="img0" src="https://i.ibb.co/TYdVNs3/Screenshot-2024-01-11-144312.png">
-                <h3>Calculator</h3>
-                <p>Made with HTML, CSS and JavaScript</p>
-                <a href="https://github.com/JsonSept/Calculator.git" target="_blank">
-                    <button>
-                    </button>
-                    Github
-                </a><br>
-            <a href="https://melodic-tanuki-6eed1b.netlify.app" target="_blank">
-                    <button>
-                    </button>
-                    Netlify
-                    </a>
-        </div>
-        </div>
-        <div class="card">
-            <div class="content">
-                <h2>03</h2>
-                <img class="img0" src="https://i.postimg.cc/TYjQHSVp/Screenshot-2024-04-05-151117.png" style="width: 230px;">
-                <h3>Keyboard</h3>
-                <p>Made with HTML, CSS and JavaScript</p>
-                <a href="https://github.com/JsonSept/jsKeyboard.git" target="_blank">
-                    <button>
-                    </button>
-                    Github
-                </a><br>
-            <a href="https://wonderful-blini-2d38a2.netlify.app" target="_blank">
-                    <button>
-                    </button>
-                    Netlify
-                    </a>
-            </div>
-        </div>
-        <div class="card">
-            <div class="content">
-                <h2>04</h2>
-                <img class="img0" src="https://i.ibb.co/dG1Bn3K/Screenshot-2024-01-11-144202.png">
-                <h3>Coffee Shop</h3>
-                <p>Made HTML and CSS</p>
-                <a href="https://github.com/JsonSept/e-commerce-website.git" target="_blank">
-                    <button>
-                    </button>
-                    Github
-                </a><br>
-            <a href="https://dynamic-liger-9dc540.netlify.app" target="_blank">
-                    <button>
-                    </button>
-                    Netlify
-                    </a>
-            </div>
-        </div>
-        <div class="card">
-            <div class="content">
-                <h2>05</h2>
-                <img class="img0" src="https://i.ibb.co/Y0VrQ5x/Screenshot-2024-01-11-144247.png">
-                <h3>My Ecommerse Website</h3>
-                <p>Made HTML, CSS and JavaScript</p>
-                <a href="https://github.com/JsonSept/newEcommerse.git" target="_blank">
-                    <button>
-                    </button>
-                    Github
-                </a><br>
-            <a href="https://sparkling-figolla-37748c.netlify.app" target="_blank">
-                    <button>
-                    </button>
-                    Netlify
-                    </a>
-            </div>
-        </div>
-        <div class="card">
-            <div class="content">
-                <h2>06</h2>
-                <img class="img0" src="https://i.ibb.co/7tkTTwB/Screenshot-2024-01-11-150844.png">
-                <h3>RGB to Hex Calculator</h3>
-                <p>Made HTML, CSS and JavaScript</p>
-                <a href="https://github.com/JsonSept/hexToRGB.git" target="_blank">
-                    <button>
-                    </button>
-                    Github
-                </a><br>
-            <a href="https://harmonious-crostata-a6bdc9.netlify.app" target="_blank">
-                    <button>
-                        
-                    </button>
-                    Netlify
-                    </a>
-            </div>
-         </div>
 
-         <div class="card">
-            <div class="content">
-                <h2>07</h2>
-                <img class="img0" src="https://i.ibb.co/9HxN7tJ/Screenshot-2024-05-03-144401.png">
-                <h3>Chowabite</h3>
-                <p>Group Presentation</p>
-                 
-                <a href="https://drive.google.com/file/d/1jGFPqlI0FkZb8b7uxTLMmrCP1U3hjz0C/view?usp=drive_link" target="_blank"><button></button>Open</a>
 
+            <div class="card">
+                <div class="content">
+                    <h2>02</h2>
+                    <img class="img0" src="https://i.ibb.co/7tkTTwB/Screenshot-2024-01-11-150844.png">
+                    <h3>RGB to Hex Calculator</h3>
+                    <p>Made HTML, CSS and JavaScript</p>
+                    <a href="https://github.com/JsonSept/hexToRGB.git" target="_blank">
+                        <button>
+                        </button>
+                        Github
+                    </a><br>
+                    <a href="https://harmonious-crostata-a6bdc9.netlify.app" target="_blank">
+                        <button>
+
+                        </button>
+                        Netlify
+                    </a>
+                </div>
             </div>
-        </div>
 
+            <div class="card">
+                <div class="content">
+                    <h2>03</h2>
+                    <img class="img0" src="https://i.ibb.co/kh5vxCV/Screenshot-2024-08-02-100836.png">
+                    <h3>Chowabite</h3>
+                    <p>Group Presentation</p>
+
+                    <a href="https://drive.google.com/file/d/1jGFPqlI0FkZb8b7uxTLMmrCP1U3hjz0C/view?usp=drive_link"
+                        target="_blank"><button></button>Open</a>
+
+                </div>
+            </div>
     </div>
     </div>
 </template>
